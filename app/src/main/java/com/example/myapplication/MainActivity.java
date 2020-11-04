cf9cffbb1e62e98324609483a6f202c310f830cc
dupre <jtd5251@uncw.edu>
2020-11-04 16:08:00 -0500

    Push 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1 = (Button) findViewById(R.id.login);
+    Button b2 = (Button) findViewById(R.id.newAccount);
+    EditText inputEmail = (EditText) findViewById(R.id.Email);
+    EditText inputPassword = (EditText) findViewById(R.id.Password);
+    FirebaseAuth firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebase = FirebaseAuth.getInstance();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                firebase.signInWithEmailAndPassword(email,password);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                firebase.createUserWithEmailAndPassword(email,password);
+            }
+        });
     }
 }
