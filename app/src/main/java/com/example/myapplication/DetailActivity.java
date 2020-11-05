21b8350b5cd2986d03fb216a18a35c20b717e37f
dupre <jtd5251@uncw.edu>
2020-11-04 23:49:03 -0500

    Push 3

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,13 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.logout);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,16 +9,33 @@ import android.widget.EditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1 = (Button) findViewById(R.id.login);
-    Button b2 = (Button) findViewById(R.id.newAccount);
-    EditText inputEmail = (EditText) findViewById(R.id.Email);
-    EditText inputPassword = (EditText) findViewById(R.id.Password);
+    Button b1;
+    Button b2;
+    EditText inputEmail;
+    EditText inputPassword;
     FirebaseAuth firebase;
+
+    public MainActivity() {
+        b1 = (Button) findViewById(R.id.loginto);
+        b2 = (Button) findViewById(R.id.newAccount);
+        inputEmail = (EditText) findViewById(R.id.Email);
+        inputPassword = (EditText) findViewById(R.id.Password);
+    }
+
+    public void onStart(){
+        super.onStart();
+        if (firebase.getCurrentUser() != null){
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            startActivity(intent);
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         firebase = FirebaseAuth.getInstance();
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
