9863e63aa9005c8084930f7f2bcd2920d5b32b80
Ian-Norris <iannorris11@gmail.com>
2020-11-18 22:07:50 -0500

    Got posts page working

--- app/src/main/java/com/example/myapplication/postView.java
+++ app/src/main/java/com/example/myapplication/postView.java
@@ -10,10 +10,19 @@ public class postView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_view);
 
+        Posts post = (Posts)getIntent().getSerializableExtra("Posts");
+
+
         TextView t1 = findViewById(R.id.itemTitle);
         TextView t2 = findViewById(R.id.textView2);
         TextView t3 = findViewById(R.id.textView3);
         TextView t4 = findViewById(R.id.textView4);
+
+        t1.setText(post.getTitle());
+        t2.setText(Double.toString(post.getPrice()));
+        t3.setText(post.getDescription());
+        t4.setText(post.getContact());
+
 
     }
 }
