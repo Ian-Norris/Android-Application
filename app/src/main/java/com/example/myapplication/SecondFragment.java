7556c9a2df3846a48cb06a83bb081397930b940b
dupre <jtd5251@uncw.edu>
2020-11-27 16:40:57 -0500

    Milestone 4 Push 11/27/2020 4:40pm

--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.io.ObjectInputStream;
+
 public class SecondFragment extends Fragment {
 
     @Override
